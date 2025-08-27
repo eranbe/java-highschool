9eb79fb65e9fbc4535ed39972cbd1f9267c0b2a1
Eran Belinsky <eran.belinsky@gmail.com>
2025-08-27 21:13:43 +0300

    Fix an embarrassing error and add a wrapper call

--- java-highschool/src/eranbe/recursion/BinarySearch.java
+++ java-highschool/src/eranbe/recursion/BinarySearch.java
@@ -20,7 +20,6 @@ public class BinarySearch {
 	}
 	public static void main(String[] args) {
 		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-		
 		System.out.println(binarySearch(arr, 8));
 		System.out.println(binarySearch(arr, 1));
 		System.out.println(binarySearch(arr, 10));
