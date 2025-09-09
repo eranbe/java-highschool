a3973476b8cd7149336a603f31105abecb811c64
Eran Belinsky <eran.belinsky@gmail.com>
2025-09-09 10:55:59 +0300

    Add documentation

--- java-highschool/src/eranbe/arrays/countersaccumulators/exercises/BirthdayParadox.java
+++ java-highschool/src/eranbe/arrays/countersaccumulators/exercises/BirthdayParadox.java
@@ -2,6 +2,11 @@ package eranbe.arrays.countersaccumulators.exercises;
 
 import java.util.Random;
 
+/**
+ * סימולציה עבור פרדוקס יום ההולדת
+ * 
+ * ימי השנה מיוצגים כמספרים בין 0 ל-364 (נתעלם משנים מעוברות)
+ */
 public class BirthdayParadox {
 
 	public static void main(String[] args) {
@@ -13,19 +18,26 @@ public class BirthdayParadox {
 		System.out.println("Average:" + average);
    }
 
+	/**
+	 * פעולה שמייצרת ימי הולדת עד שיש התנגשות, ומחזירה כמה ימי הולדת נוצרו
+	 */
 	public static int randomizeBirthdays() {
 		Random rand = new Random();
         int daysInYear = 365;
         boolean[] daysOfYear = new boolean[daysInYear];
-        boolean hasCollision = false;
         int count = 0;
+        boolean hasCollision = false;
 
         while (!hasCollision) {
+        	// נגריל יום הולדת
             int day = rand.nextInt(daysInYear);
+            // נגדיל את מונה הימים שהגרלנו
             count++;
+            // האם יש התנגשות של ימי הולדת? האם כבר היה לנו יום הולדת ביום זה?
             if (daysOfYear[day]) {
             	hasCollision = true;
             } else {
+            	// נסמן שראינו יום הולדת ביום זה
             	daysOfYear[day] = true;
             }
         }
