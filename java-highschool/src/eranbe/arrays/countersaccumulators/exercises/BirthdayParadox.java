5974caca1d3d42b0348970d085e55e0d19439c3c
Eran Belinsky <eran.belinsky@gmail.com>
2025-09-09 11:47:33 +0300

    עוד דוקומנטציה

--- java-highschool/src/eranbe/arrays/countersaccumulators/exercises/BirthdayParadox.java
+++ java-highschool/src/eranbe/arrays/countersaccumulators/exercises/BirthdayParadox.java
@@ -6,15 +6,22 @@ import java.util.Random;
  * סימולציה עבור פרדוקס יום ההולדת
  * 
  * ימי השנה מיוצגים כמספרים בין 0 ל-364 (נתעלם משנים מעוברות)
+ * 
+ * התוצאה הצפויה היא שורש של (פיי כפול 365 חלקי 2), שזה יוצא בערך 24 https://www.google.com/search?q=sqrt(pi*365%2F2)
+ * 
+ * עוד מידע על פרדוקס יום ההולדת: 
+ * https://he.wikipedia.org/wiki/%D7%A4%D7%A8%D7%93%D7%95%D7%A7%D7%A1_%D7%99%D7%95%D7%9D_%D7%94%D7%94%D7%95%D7%9C%D7%93%D7%AA
+ * https://www.youtube.com/watch?v=KtT_cgMzHx8
  */
 public class BirthdayParadox {
 
 	public static void main(String[] args) {
+		// נקרא לפעולה המון פעמים ונחשב את המספר הממוצע של ימי הולדת עד להתנגשות
 		int sum = 0;
-		for (int i = 0; i < 100000; i++) {
+		for (int i = 0; i < 1000000; i++) {
 			sum += randomizeBirthdays();
 		}
-		double average = sum / 100000.0;
+		double average = sum / 1000000.0;
 		System.out.println("Average:" + average);
    }
 
