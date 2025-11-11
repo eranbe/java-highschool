c35ecd68ed5fd8e39accf20fa9bc30da19e212b9
Eran Belinsky <eran.belinsky@gmail.com>
2025-11-11 17:52:23 +0200

    Add a more conventional max calculation

--- java-highschool/src/eranbe/classes/compoundclasses/busanddriver/BusLine.java
+++ java-highschool/src/eranbe/classes/compoundclasses/busanddriver/BusLine.java
@@ -31,12 +31,29 @@ public class BusLine {
 		return allLines[idx].getDriver().getDriverName();
 	}
 	
-	public static void main(String[] args) {
+	// מימוש נוסף, יותר סטנדרטי, למציאת מקסימום
+	public static String findMostSeniorDriverName2(BusLine[] allLines) {
+		String mostSeniorName = "";
+        int maxSeniority = -1;
+
+        for (int i = 0; i < allLines.length; i++) {
+            Driver driver = allLines[i].getDriver();
+            int seniority = driver.getSeniority();
+
+            if (seniority > maxSeniority) {
+                maxSeniority = seniority;
+                mostSeniorName = driver.getDriverName();
+            }
+        }
+        return mostSeniorName;
+	}
+
+    public static void main(String[] args) {
 		BusLine[] allLines = {
 				new BusLine(1, new Driver("John", 10)),
 				new BusLine(2, new Driver("Paul", 20)),
 				new BusLine(3, new Driver("George", 25)),
 				new BusLine(4, new Driver("Ringo", 18)) };
-		System.out.println(findMostSeniorDriverName(allLines));
+		System.out.println(findMostSeniorDriverName2(allLines));
 	}
 }
