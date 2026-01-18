9fdb10f54595ac16982ce52527b6f9325c7d0180
Eran Belinsky <eran.belinsky@gmail.com>
2026-01-18 21:46:37 +0200

    Add 3 more solutions

--- java-highschool/src/eranbe/bagrut/q899371/a2025/q2/Arrange.java
+++ java-highschool/src/eranbe/bagrut/q899371/a2025/q2/Arrange.java
@@ -2,8 +2,27 @@ package eranbe.bagrut.q899371.a2025.q2;
 
 public class Arrange {
 	public static void main(String[] args) {
-		int[] input = {7, -2, -5, 61, 4, -1, 0, 33 };
+		int[] input = createArray();
 		arrange(input);
+		printArray(input);
+		input = createArray();
+		arrange2(input);
+		printArray(input);
+		input = createArray();
+		arrange3(input);
+		printArray(input);
+		input = createArray();
+		arrange4(input);
+		printArray(input);
+
+	}
+
+	public static int[] createArray() {
+		int[] input = {7, -2, -5, 61, 4, -1, 0, 33 };
+		return input;
+	}
+
+	public static void printArray(int[] input) {
 		for (int i = 0; i < input.length; i++) {
 			System.out.print(input[i] + ", ");
 		}
@@ -16,10 +35,6 @@ public class Arrange {
 		}
 		int[] positives = new int[input.length];
 		int[] negatives = new int[input.length];
-		for (int i = 0; i < input.length; i++) {
-			positives[i] = -1;
-			negatives[i] = 1;
-		}
 		int pIdx = 0;
 		int nIdx = 0;
 		for (int i = 0; i < input.length; i++) {
@@ -36,4 +51,79 @@ public class Arrange {
 			input[i+pIdx] = negatives[i];
 		}
 	}
+	
+	public static void arrange2(int[] input) {
+		if (input == null || input.length < 2) {
+			return;
+		}
+		int[] positives = new int[input.length];
+		int[] negatives = new int[input.length];
+		for (int i = 0; i < input.length; i++) {
+			positives[i] = -1;
+			negatives[i] = 1;
+		}
+		int pIdx = 0;
+		int nIdx = 0;
+		for (int i = 0; i < input.length; i++) {
+			if (input[i] >= 0) {
+				positives[pIdx] = input[i];
+				pIdx++;
+			} else {
+				negatives[nIdx] = input[i];
+				nIdx++;
+			}
+		}
+		int i = 0;
+		while (positives[i] != -1) {
+			input[i] = positives[i];
+			i++;
+		}
+		int j = 0;
+		while (negatives[j] != 1) {
+			input[i] = negatives[j];
+			i++;
+			j++;
+		}
+	}
+
+	public static void arrange3(int[] input) {
+		if (input == null || input.length < 2) {
+			return;
+		}
+		int[] helper = new int[input.length];
+		int idx = 0;
+		for (int i = 0; i < input.length; i++) {
+			if (input[i] >= 0) {
+				helper[idx] = input[i];
+				idx++;
+			}
+		}
+		for (int i = 0; i < input.length; i++) {
+			if (input[i] < 0) {
+				helper[idx] = input[i];
+				idx++;
+			}
+		}
+		for (int i = 0; i < helper.length; i++) {
+			input[i] = helper[i];
+		}
+	}
+	
+	public static void arrange4(int[] input) {
+		if (input == null || input.length < 2) {
+			return;
+		}
+		int insertPos = 0;
+		for (int i = 0; i < input.length; i++) {
+			if (input[i] >= 0) {
+				int temp = input[i];
+				for (int j = i; j > insertPos; j--) {
+					input[j] = input[j - 1];
+				}
+				input[insertPos] = temp;
+				insertPos++;
+			}
+		}
+	}
+
 }
