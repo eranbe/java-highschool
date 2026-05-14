0396333c026d455331309e97e7c5aea5bd485478
Eran Belinsky <eran.belinsky@gmail.com>
2026-05-14 11:19:48 +0300

    Galton Board - good for arrays + static methods + nested loops

---
+++ java-highschool/src/eranbe/arrays/GaltonBoard.java
@@ -0,0 +1,63 @@
+package eranbe.arrays;
+
+import java.util.Random;
+
+public class GaltonBoard {
+    public static void main(String[] args) {
+        // הרצת הסימולציה עם פרמטרים משתנים
+        int[] simulation1 = runGaltonSimulation(10, 500);
+        System.out.println("--- לוח של  10 שורות ---");
+        printBins(simulation1);
+
+        int[] simulation2 = runGaltonSimulation(20, 1000);
+        System.out.println("--- לוח של  20 שורות ---");
+        printBins(simulation2);
+}
+
+    /**
+     * פעולה גנרית להרצת סימולציית לוח גלטון
+     * @param rows מספר שורות הפינים בלוח
+     * @param numBalls מספר הכדורים להרצה
+     * @return מערך המכיל את התפלגות הכדורים בתאים הסופיים
+     */
+    public static int[] runGaltonSimulation(int rows, int numBalls) {
+        Random rnd = new Random();
+        int[] bins = new int[rows + 1];
+
+        for (int b = 0; b < numBalls; b++) {
+            int rightTurns = 0;
+            for (int r = 0; r < rows; r++) {
+                if (rnd.nextInt(2) == 1) {
+                    rightTurns++;
+                }
+            }
+            bins[rightTurns]++;
+        }
+        return bins;
+    }
+
+    /**
+     * פעולה שמנהלת את הדפסת כל הלוח
+     * @param bins המערך הסופי של הסימולציה
+     */
+    public static void printBins(int[] bins) {
+        for (int i = 0; i < bins.length; i++) {
+            System.out.print("Bin ");
+            if (i < 10 ) {
+            	System.out.print(" ");
+            }
+            System.out.print(i + ": ");
+            for (int j = 0; j < bins[i]; j++) {
+                System.out.print("*");
+            }
+            System.out.println(); // ירידת שורה בסוף ההדפסה
+        }
+    }
+
+    /**
+     * פעולה שמדפיסה שורת כוכביות לפי מספר נתון
+     * @param count מספר הכוכביות להדפסה
+     */
+    public static void printRow(int count) {
+    }
+}
